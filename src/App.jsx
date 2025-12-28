5f0fd0937bd2a126567349e99f5e85bd28595d2a
Dhyaneswar Bachu <dhyanuthewar@gmail.com>
2025-12-29 04:07:30 +0530

    create delete cart item feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import './App.css'
 
 function App() {
   const [cart, setCart] = useState([]);
-      const loadCart = async () => {
-      const response = await axios.get('/api/cart-items?expand=product');
-      setCart(response.data);
-    };
+
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  };
+
   useEffect(() => {
     loadCart();
   }, []);
@@ -19,7 +21,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
   )
--- src/pages/checkout/DeliveryOptions.jsx
+++ src/pages/checkout/DeliveryOptions.jsx
@@ -1,17 +1,13 @@
 import dayjs from 'dayjs';
+import axios from 'axios';
 import { formatMoney } from '../../utils/money';
 
-export function DeliveryOptions({
-  cartItem,
-  deliveryOptions,
-  onUpdateDeliveryOption
-}) {
+export function DeliveryOptions({ cartItem, deliveryOptions, loadCart }) {
   return (
     <div className="delivery-options">
       <div className="delivery-options-title">
         Choose a delivery option:
       </div>
-
       {deliveryOptions.map((deliveryOption) => {
         let priceString = 'FREE Shipping';
 
@@ -19,25 +15,24 @@ export function DeliveryOptions({
           priceString = `${formatMoney(deliveryOption.priceCents)} - Shipping`;
         }
 
-        return (
-          <div key={deliveryOption.id} className="delivery-option">
-            <input
-              type="radio"
-              className="delivery-option-input"
-              name={`delivery-option-${cartItem.productId}`}
-              checked={deliveryOption.id === cartItem.deliveryOptionId}
-              onChange={() =>
-                onUpdateDeliveryOption(
-                  cartItem.productId,
-                  deliveryOption.id
-                )
-              }
-            />
+        const updateDeliveryOption = async () => {
+          await axios.put(`/api/cart-items/${cartItem.productId}`, {
+            deliveryOptionId: deliveryOption.id
+          });
+          await loadCart();
+        };
 
+        return (
+          <div key={deliveryOption.id} className="delivery-option"
+            onClick={updateDeliveryOption}>
+            <input type="radio"
+              checked={deliveryOption.id === cartItem.deliveryOptionId}
+              onChange={() => {}}
+              className="delivery-option-input"
+              name={`delivery-option-${cartItem.productId}`} />
             <div>
               <div className="delivery-option-date">
-                {dayjs(deliveryOption.estimatedDeliveryTimeMs)
-                  .format('dddd, MMMM D')}
+                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
               </div>
               <div className="delivery-option-price">
                 {priceString}
